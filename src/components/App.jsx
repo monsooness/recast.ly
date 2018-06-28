b914cc88844b3451dfdd3714a56d52d70b031701
monsooness <monsooness>
2018-06-28 11:13:50 -0700

    Made App a stateful class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,28 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><Search/></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><VideoPlayer/></div>
-      </div>
-      <div className="col-md-5">
-        <div><VideoList videos={window.exampleVideoData}/></div>
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+  }
 
+  render() {
+    return(
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search/></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer/></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList videos={window.exampleVideoData}/></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
